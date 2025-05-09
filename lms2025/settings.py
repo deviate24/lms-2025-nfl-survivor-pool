348efb9c1dea9bd8e143ae24733ac35893dea254
deviate24 <deviatenft@gmail.com>
2025-05-09 14:46:24 -0700

    Update settings.py to match PythonAnywhere configuration

--- lms2025/settings.py
+++ lms2025/settings.py
@@ -18,13 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-devel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'deviate.pythonanywhere.com',
-    'lms2025.xyz',
-    'www.lms2025.xyz',
-]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'deviate.pythonanywhere.com', 'lms2025.xyz', 'www.lms2025.xyz']
 
 # Application definition
 
